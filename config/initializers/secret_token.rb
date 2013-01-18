6f6a06d0b0365f764383d24f856a9aa5b95dd9a4
Mark Kadlec <kadlecmark@hotmail.com>
2013-01-17 23:23:21 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeCodersQuorum::Application.config.secret_token = '04ea4ac0e8a377861c7eb6022d013eb5eef4c7bae9692e10885273f36c9705fff4591817112bd4e4ea18bd866b1273b315343aa9ed83f1edc8fb769465fd5af4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeCodersQuorum::Application.config.session_store :cookie_store, key: '_CoffeeCodersQuorum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CoffeeCodersQuorum::Application.config.session_store :active_record_store
