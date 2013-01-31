53669ead2a80eb20ca7a0b74e6ed0923c9d02edf
Mark Kadlec <kadlecmark@hotmail.com>
2013-01-31 02:48:02 -0700

    Removed the InsertAdminUser migration from project

--- db/migrate/20130130034658_insert_admin_user.rb
+++
@@ -1,9 +0,0 @@
-class InsertAdminUser < ActiveRecord::Migration
-  def up
-  	User.create!(:email => "mpk@mpk.com", :name => "admin", :password => "cough", :admin => true)
-  end
-
-  def down
-  	User.delete_all(:email => "mpk@mpk.com")
-  end
-end
