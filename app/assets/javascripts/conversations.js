63b955a5084586b103886841596b0b57429e4b46
Mark Kadlec <kadlecmark@hotmail.com>
2013-02-07 18:18:47 -0700

    Styled delete

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -2,15 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$(document).ready(function() {
-	$('#addComment').click(function() {
-		$('#new_comment').toggle('fast');
-	});
-
-	$('#new_comment').hide();
-
-	$("#new_comment").bind('ajax:success', function(evt, data, status, xhr) {
-		$('#commentsDiv').append($(xhr.responseText)).show('fast');
-	});
-
-});
